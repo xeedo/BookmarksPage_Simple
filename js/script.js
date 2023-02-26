8514b028a8193ab0566d2ad15e0c5b9c2f3a6759
Przemysław Grądzik <pggradzik@gmail.com>
2023-02-26 16:46:37 +0100

    Simplified page

--- js/script.js
+++ js/script.js
@@ -4,44 +4,13 @@ let linkList = document.getElementById("linkList")
 const navItemList = [
 	{
 	text: "Category no. 1", 
-	ev: "filterLinks('Category no. 1')"
+	link: ""
 	}, 
 	
 	{
 	text: "Category no. 2", 
-	ev: "filterLinks('Category no. 2')"
+	link: ""
 	}
-]
-
-
-const linkItemList = [
-	{
-	cat: navItemList[0].text, 
-	text: "Link no. 1", 
-    link: "", 
-	desc: "Short description of link no. 1"
-	}, 
-	
-	{
-	cat: navItemList[0].text, 
-	text: "Link no. 1", 
-    link: "", 
-	desc: "Short description of link no. 2"
-	}, 
-	
-	{
-	cat: navItemList[1].text, 
-	text: "Link no. 3", 
-    link: "", 
-	desc: "Short description of link no. 3"
-	}, 
-	
-	{
-	cat: navItemList[1].text, 
-	text: "Link no. 4", 
-    link: "", 
-	desc: "Short description of link no. 4"
-	} 
 ]
 
 for (item of navItemList) {
@@ -58,36 +27,8 @@ function addLinks(element, nList) {
 	if (element.hasOwnProperty("href")) {
 		linkItem.href = element.link
 	}
-    navItem.appendChild(linkItem)
-
-    if (element.hasOwnProperty("desc")) {
-		let descItem = document.createElement("p")
-		
-        descItem.innerHTML = element.desc
-        navItem.appendChild(descItem)
-    }
 	
-	if (element.hasOwnProperty("ev")) {
-		navItem.setAttribute('onclick', element.ev)
-	}
+    navItem.appendChild(linkItem)
     
     nList.appendChild(navItem)
-}
-
-function filterLinks(category) {
-	const itemList = []
-	
-	while (linkList.hasChildNodes()) {
-		linkList.removeChild(linkList.firstChild)
-	}
-	
-	for (item of linkItemList) {
-		if (item.cat === category) {
-			itemList.push(item)
-		}
-	}
-	
-	for (item of itemList) {
-		addLinks(item, linkList)
-	}
 }
